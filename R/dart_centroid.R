16ac420af5e3e139e7abbac31d0fc51de138e1b3
s3818311 <s3818311@rmit.edu.vn>
2022-06-17 23:06:42 +0700

    add roxygen documentation

--- R/dart_centroid.R
+++ R/dart_centroid.R
@@ -1,6 +1,21 @@
-library(sf)
-
+#' sf::st_centroid() wrapper with optional CRS transformation
+#'
+#' @param sf sf object
+#' @param target_crs CRS which the centroid calculation will be performed with (optional)
+#'
+#' @return original sf object with new centroid column
 #' @export
+#'
+#' @examples
+#' \dontrun{
+#' # read in gson vector
+#' hcmc_gson <- st_read(
+#'   readChar(gson_fname, file.info(gson_fname)$size),
+#'   quiet = TRUE
+#' )
+#'
+#' hcmc_gson_w_centroid <- dart_centroid(hcmc_gson, 9210)
+#' }
 dart_centroid <- function(sf, target_crs = 4326) {
     sf %>% mutate(
         centroid = sf::st_transform(sf, sf::st_crs(target_crs)) %>%
