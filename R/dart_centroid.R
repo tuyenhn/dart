90c56e9c332d92f56a779177a3e2e6beffca4dba
s3818311 <s3818311@rmit.edu.vn>
2022-06-23 23:30:46 +0700

    issue #2

--- R/dart_centroid.R
+++ R/dart_centroid.R
@@ -20,7 +20,6 @@
 dart_centroid <- function(sf, target_crs = 4326) {
     sf %>% mutate(
         centroid = sf::st_transform(sf, sf::st_crs(target_crs)) %>%
-            sf::st_geometry() %>%
             sf::st_centroid() %>%
             sf::st_transform(sf::st_crs(4326))
     )
