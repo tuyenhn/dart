d001fca2c96530a5156de95449c2381c19dc45b1
s3818311 <s3818311@rmit.edu.vn>
2022-07-13 22:17:05 +0700

    compromise on issue #3 and #4

--- R/dart_centroid.R
+++ R/dart_centroid.R
@@ -17,25 +17,29 @@
 #'
 #' hcmc_gson_w_centroid <- dart_centroid(hcmc_gson, 9210)
 #' }
-dart_centroid <- function(sf, target_crs = 4326) {
+dart_centroid <- function(sf, target_crs = 4326, output_crs = 4326) {
     if (!class(sf)[1] == "sf") {
         stop("sf must be of class `sf`", call. = FALSE)
     }
     if (class(target_crs) != "numeric") {
         stop("target_crs must be a number", call. = FALSE)
     }
+    if (class(output_crs) != "numeric") {
+        stop("output_crs must be a number", call. = FALSE)
+    }
 
     dart_centroid_(
         sf = sf,
-        target_crs = target_crs
+        target_crs = target_crs,
+        output_crs = output_crs
     )
 }
 
-dart_centroid_ <- function(sf, target_crs) {
+dart_centroid_ <- function(sf, target_crs, output_crs) {
     sf %>% mutate(
         centroid = sf::st_transform(sf, sf::st_crs(target_crs)) %>%
             sf::st_centroid() %>%
-            sf::st_transform(sf::st_crs(4326)) %>%
+            sf::st_transform(sf::st_crs(output_crs)) %>%
             .$geometry
     )
 }
